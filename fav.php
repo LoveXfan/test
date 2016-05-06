7bb8b02187b53d1ae4f22986c747966d81234171
LoveXfan <xfan123@126.com>
2016-05-06 14:05:34 +0800

    收藏文件

---
+++ fav.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>个人中心-消息通知</title>
+	<link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+	<?php require("header.php") ?>
+	<!-- 页面主休主内容 -->
+	<div class="mainContent">
+		<div class="wrap clearfix">
+			<?php require_once("user_info.php"); ?>
+			
+			<!-- 列表切换 -->
+			<ul class="tab-menu-lg">
+				<li><a href="index.php"><i class="icon-20 msg"></i>消息通知</a></li>
+				<li><a href="source.php"><i class="icon-20 source"></i>源站管理</a></li>
+				<li><a href="recommend.php"><i class="icon-20 star"></i>我的推荐</a></li>
+				<li class="action"><a href="fav.php"><i class="icon-20 heart"></i>我的收藏</a></li>
+			</ul>
+			<div class="user-content">
+				<!-- 源管理 -->
+				<div class="user-side float-left">
+					<ul class="user-menu">
+						<li class="action"><a href="#">商业要闻</a></li>
+						<li><a href="#">电子商务</a></li>
+						<li><a href="#">云服务器</a></li>
+						<li><a href="#">电子商务</a></li>
+						<li><a href="#">商业数据</a></li>
+						<li><a href="#">移动应用</a></li>
+						<li><a href="#">商业营销</a></li>
+						<li><a href="#">智能硬件</a></li>
+						<li><a href="#">数据中心</a></li>
+						<li><a href="#">移动互联网</a></li>
+					</ul>
+				</div>
+				<div class="user-body clearfix">
+					<div class="ui-list">
+						<!-- 图片+标题+引文 -->
+							<dl>
+								<dd>
+									<h2><a href="http://info.thaimi.net/index/click_log/120?id%3D90384%26sid%3D57%26url%3Dhttp%3A%2F%2Fwww.ccstock.cn%2Ffinance%2Fhangyedongtai%2F2016-05-04%2FA1462296173845.html%26token%3D23b8e220b7b8d1dcd0b5043734699689" target="_blank" title="一季度国内并购交易回落 金融行业交易规模居首位">一季度国内并购交易回落 金融行业交易规模居首位</a></h2>
+									<p class="text"> &nbsp;&nbsp;&nbsp;&nbsp;投中数据显示，今年一季度中国并购市场交易规模为689.9亿美元，环比下降66.19%，同比下降54.80%&nbsp;&nbsp;&nbsp;&nbsp;■本报记者 李文&nbsp;&nbsp;&nbsp;&nbsp;在经历了去年</p>
+					
+									<p class="meta">
+										<span>证券日报</span>
+										<span><i class="icon eye"></i>0</span>
+										<a href="javascript:void(0);" class="float-right share" data-id="90384"><i class="icon share"></i></a>
+										<a href="javascript:void(0);" class="float-right fav " data-id="90384"><i class="icon heard"></i><span>0</span></a>
+									</p>
+								</dd>
+				
+								<dd>
+									<h2><a href="http://info.thaimi.net/index/click_log/120?id%3D90385%26sid%3D57%26url%3Dhttp%3A%2F%2Fwww.ccstock.cn%2Ffinance%2Fhongguanjingji%2F2016-05-04%2FA1462318121724.html%26token%3Dfbb541647dad5591183bda5d84b1a03a" target="_blank" title="多种货币政策工具持续混搭 央行开展千亿逆回购">多种货币政策工具持续混搭 央行开展千亿逆回购</a></h2>
+										<p class="text"> &nbsp; &nbsp; 记者 王媛 编辑 浦泓毅&nbsp;&nbsp;&nbsp;&nbsp;央行继续多管齐下保持银行间市场流动性的合理充裕。本月首个交易日，央行便在公开市场开展千亿规模的7天逆回购操作。同时，央行昨日公布，从2016年5月起，在每月月初对国家开发银行、中</p>
+									<p class="meta">
+										<span>证券日报</span>
+										<span><i class="icon eye"></i>0</span>
+										<a href="javascript:void(0);" class="float-right share" data-id="90385"><i class="icon share"></i></a>
+										<a href="javascript:void(0);" class="float-right fav " data-id="90385"><i class="icon heard"></i><span>0</span></a>
+									</p>
+								</dd>
+								<dd>
+									
+									<h2><a href="http://info.thaimi.net/index/click_log/120?id%3D90390%26sid%3D57%26url%3Dhttp%3A%2F%2Fwww.ccstock.cn%2Ffinance%2Fhangyedongtai%2F2016-05-04%2FA1462313486376.html%26token%3D995f57c5b04d5c1e2204cc1324ff9524" target="_blank" title="“XX金服”已达17家 小玩家恐成陪跑">“XX金服”已达17家 小玩家恐成陪跑</a></h2>
+									<p class="text"> &nbsp;&nbsp;&nbsp;&nbsp;证券时报记者刘筱攸&nbsp;&nbsp;&nbsp;&nbsp;换个角度看我国的互联网金融领域，欢乐真的很多。&nbsp;&nbsp;&nbsp;&nbsp;比如在相当长一段时间内，“互联网金融”这个词几</p>
+									<p class="meta">
+										<span>证券日报</span>
+										<span><i class="icon eye"></i>0</span>
+										<a href="javascript:void(0);" class="float-right share" data-id="90390"><i class="icon share"></i></a>
+										<a href="javascript:void(0);" class="float-right fav " data-id="90390"><i class="icon heard"></i><span>0</span></a>
+									</p>
+								</dd>
+							</dl>
+					</div>
+					<div class="text-right margin-tb">
+						<ul class="pager">
+							<li><a href="#article?page=12" data-page="12"><i class="icon arrow-left"></i></a></li>
+							<li class="action"><a href="#article?page=1" data-page="1">1</a></li>
+							<li><a href="#article?page=2" data-page="2">2</a></li>
+							<li><a href="#article?page=3" data-page="3">3</a></li>
+							<li class="disabled"><a href="javascript:;">…</a></li>
+							<li><a href="#article?page=10" data-page="10">10</a></li>
+							<li><a href="#article?page=10" data-page="10">11</a></li>
+							<li><a href="#article?page=10" data-page="10">12</a></li>
+							<li><a href="#article?page=12" data-page="12"><i class="icon arrow-right"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+
+	<?php require("footer.php") ?>
+</body>
+</html>
